e68e126abf199185dc4d1e07f1277cba5b220c67
nimomeng <egeg3000@126.com>
2017-04-19 21:20:16 +0800

    change UI Style of Find My Postion Demo

--- 06.Find My Position/FindMyPosition/FindMyPosition/ViewController.swift
+++ 06.Find My Position/FindMyPosition/FindMyPosition/ViewController.swift
@@ -17,23 +17,43 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     let locationStrLabel = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
+        let bgImageView = UIImageView(frame: self.view.bounds)
+        bgImageView.image = #imageLiteral(resourceName: "phoneBg")
+        self.view.addSubview(bgImageView)
+        
+        let blurEffect: UIBlurEffect = UIBlurEffect(style: .light)
+        let blurView: UIVisualEffectView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = self.view.bounds
+        self.view.addSubview(blurView)
         
         locationManager.delegate = self
-        locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
         
-        locationLabel.frame = CGRect(x: 0, y: 100, width: 400, height: 100)
-        locationLabel.numberOfLines = 3
+        locationLabel.frame = CGRect(x: 0, y: 50, width: self.view.bounds.width, height: 100)
+        locationLabel.textAlignment = .center
+        locationLabel.textColor = UIColor.white
         self.view.addSubview(locationLabel)
-        locationStrLabel.frame = CGRect(x: 0, y: 200, width: 400, height: 100)
-        locationStrLabel.numberOfLines = 5
+        
+        locationStrLabel.frame = CGRect(x: 0, y: 100, width: self.view.bounds.width, height: 50)
+        locationStrLabel.textAlignment = .center
+        locationStrLabel.textColor = UIColor.white
         self.view.addSubview(locationStrLabel)
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let findMyLocationBtn = UIButton(type: .custom)
+        findMyLocationBtn.frame = CGRect(x: 50, y: self.view.bounds.height - 80, width: self.view.bounds.width - 100, height: 50)
+        findMyLocationBtn.setTitle("Find My Position", for: UIControlState.normal)
+        findMyLocationBtn.setTitleColor(UIColor.white, for: UIControlState.normal)
+        findMyLocationBtn.addTarget(self, action: #selector(findMyLocation), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(findMyLocationBtn)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func findMyLocation() {
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -55,10 +75,9 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
                 let addressDictionary = mark.addressDictionary! as NSDictionary
                 let country = addressDictionary.value(forKey: "Country") as! String
                 let city = addressDictionary.object(forKey: "City") as! String
-                let subLocation = addressDictionary.object(forKey: "SubLocality") as! String
                 let street = addressDictionary.object(forKey: "Street") as! String
                 
-                let finalAddress = "Country: \(country) ; City: \(city) ; Sublocation: \(subLocation) ; Street: \(street)"
+                let finalAddress = "\(street),\(city),\(country)"
                 self.locationStrLabel.text = finalAddress
 
             }
