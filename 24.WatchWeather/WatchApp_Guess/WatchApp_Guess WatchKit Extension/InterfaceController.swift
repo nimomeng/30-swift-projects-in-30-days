31378a2b3b4f4c8b0e19f3575b5a0f97101b0445
nimomeng <egeg3000@126.com>
2017-05-20 22:01:27 +0800

    finished apple watch app

---
+++ 24.WatchWeather/WatchApp_Guess/WatchApp_Guess WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,66 @@
+//
+//  InterfaceController.swift
+//  WatchApp_Guess WatchKit Extension
+//
+//  Created by nimo on 2017/5/20.
+//  Copyright © 2017年 nimoAndHisFriends. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import WatchConnectivity
+
+class InterfaceController: WKInterfaceController,WCSessionDelegate {
+    @IBOutlet var resultSlider: WKInterfaceSlider!
+    @IBOutlet var resultLabel: WKInterfaceLabel!
+    @IBOutlet var resultConfirmButton: WKInterfaceButton!
+
+    @IBOutlet var resultOfGuess: WKInterfaceLabel!
+    
+    var numberToBeGuessed: Int!
+    var numberOfSlider: Int = 3
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    @IBAction func sliderUpdate(_ value: Float) {
+        numberOfSlider = Int(value * 5)
+        resultLabel.setText("Your guess: \(numberOfSlider)")
+    }
+    
+    @IBAction func guessAction() {
+        if numberToBeGuessed == nil {
+            resultOfGuess.setText("Open iPhone App first")
+        }
+        else if numberOfSlider == numberToBeGuessed {
+            resultOfGuess.setText("回答正确❤️")
+        }
+        else {
+            resultOfGuess.setText("再猜一次💔")
+        }
+    }
+    
+    override func willActivate() {
+        if WCSession.isSupported() {
+            let session = WCSession.default()
+            session.delegate = self
+            session.activate()
+        }
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    }
+    
+    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
+        numberToBeGuessed = applicationContext["numberToBeGuessed"] as! Int
+    }
+}
---
+++ 24.WatchWeather/WatchApp_Guess/WatchApp_Guess/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  WatchApp_Guess
+//
+//  Created by nimo on 2017/5/20.
+//  Copyright © 2017年 nimoAndHisFriends. All rights reserved.
+//
+
+import UIKit
+import WatchConnectivity
+
+class ViewController: UIViewController,WCSessionDelegate {
+
+    var wcsession: WCSession!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let generateNumberBtn = UIButton(type: .custom)
+        generateNumberBtn.frame = CGRect(x: 10, y: 10, width: 200, height: 50)
+        generateNumberBtn.setTitle("Generate Number", for: .normal)
+        generateNumberBtn.setTitleColor(UIColor.black, for: .normal)
+        self.view.addSubview(generateNumberBtn)
+        generateNumberBtn.center = self.view.center
+        generateNumberBtn.addTarget(self, action: #selector(generateNumber), for: .touchUpInside)
+        generateNumberBtn.setTitleColor(UIColor.red, for: .highlighted)
+        generateNumberBtn.setTitleColor(UIColor.green, for: .selected)
+        generateNumberBtn.layer.borderColor = UIColor.black.cgColor
+        generateNumberBtn.layer.borderWidth = 1
+        generateNumberBtn.layer.cornerRadius = 8
+        
+        wcsession = WCSession.default()
+        if WCSession.isSupported() {
+            wcsession.delegate = self
+            wcsession.activate()
+        }
+        
+        self.view.backgroundColor = UIColor.yellow
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func generateNumber() {
+        let number:Int = Int(arc4random()) % 5 + 1
+        do {
+            try wcsession.updateApplicationContext(["numberToBeGuessed": number])
+        } catch {
+            print("Error Happened!")
+        }
+        
+        print("")
+//        todo send to apple watch
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        
+    }
+
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("")
+    }
+}
+
