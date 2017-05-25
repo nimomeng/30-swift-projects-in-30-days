3801e22bcbd4813e9d2f14515ab68b62b905fd43
nimomeng <egeg3000@126.com>
2017-05-26 00:10:14 +0800

    add gifs

--- 02.CustomFont/CustomFont/ViewController.swift
+++ 02.CustomFont/CustomFont/ViewController.swift
@@ -14,22 +14,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = "I am the king of the world,ha ha ha ha ha"
+        label.text = "I am the king of the world"
         label.textAlignment = NSTextAlignment.center
         self.view.addSubview(label)
         label.snp.makeConstraints { (make) in
             make.top.equalTo(100)
             make.centerX.equalTo(self.view)
         }
+        label.font = UIFont.systemFont(ofSize: 30)
         
-        let changeBtn = UIButton()
+        let changeBtn = UIButton(type: .custom)
         changeBtn.setTitle("Change Font Family", for: UIControlState.normal)
         changeBtn.addTarget(self, action: #selector(changeFontFamily), for: UIControlEvents.touchUpInside)
         changeBtn.setTitleColor(UIColor.blue, for: UIControlState.normal)
         self.view.addSubview(changeBtn)
+        changeBtn.layer.borderColor = UIColor.blue.cgColor
+        changeBtn.layer.borderWidth = 1
+        changeBtn.layer.cornerRadius = 5
         changeBtn.snp.makeConstraints { (make) in
             make.top.equalTo(500)
             make.centerX.equalTo(self.view)
+            make.width.equalTo(200)
         }
         printAllSupportedFontNames()
     }
@@ -40,7 +45,7 @@ class ViewController: UIViewController {
     }
 
     func changeFontFamily() {
-        label.font = UIFont(name: "Savoye LET", size: 17)
+        label.font = UIFont(name: "Savoye LET", size: 30)
     }
     
     func printAllSupportedFontNames() {
