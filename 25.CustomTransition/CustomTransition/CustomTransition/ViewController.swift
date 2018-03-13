9136ada21a73459c8b75efe6574c151dc89000fe
macbook pro yifan <yifan@magicmoma.com>
2018-03-13 18:01:53 +0800

    fix: 修改 nav 的 delegate 赋值时机，解决 Detail 页面滑动部分取消后，再此滑动的 bug

--- 25.CustomTransition/CustomTransition/CustomTransition/ViewController.swift
+++ 25.CustomTransition/CustomTransition/CustomTransition/ViewController.swift
@@ -33,8 +33,8 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         }
 }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         self.navigationController?.delegate = self
         self.navigationController?.isNavigationBarHidden = true
         collectionView.reloadData()
